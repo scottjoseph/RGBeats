913889a37a7c13fc2ece5a5be137c0423e93895d
Joseph Scott <email": null>
2016-04-08 08:56:17 -0500

    add more songs

--- public/js/application.js
+++ public/js/application.js
@@ -77,26 +77,70 @@ $(document).ready(function() {
 
     var red = $("#ember241").val()
     var green = $("#ember242").val()
+
     var blue = $("#ember243").val()
-    // debugger
-51, 102, 153, 204, 255
+// 51, 102, 153, 204, 255
+    var trackGenre = ""
     var trackId = ""
     if (red > 204){
-      //rock
-      trackId = "5VVWgWH8HFLAtM8lbttvn9"
+      trackGenre = "rock"
     } else if (red > 153){
-      //rap | hip-hop
-      trackId = "5dX9fDuiEkcEg12v6JQ4iO"
+      trackGenre = "rap"
     } else if (red > 102){
-      //pop
-      trackId = "455AfCsOhhLPRc68sE01D8"
+      trackGenre = "pop"
     } else if (red > 51) {
-      //country
-      trackId = "1WzAeadSKJhqykZFbJNmQv"
+      trackGenre = "country"
     } else {
-      //dance
-      trackId = "4jacsL77ZYnpInmTtUBaJW"
+      trackGenre = "dance"
     };
+
+    switch(trackGenre) {
+      case "rock":
+        if (green > blue){
+          trackId = "3otkWCw3GxRkdBkJK18OsJ";
+         } else if (green < blue) {
+          trackId = "0grFc6klR3hxoHLcgCYsF4";
+         } else {
+          trackId = "10zlwR7kvVbD9OBkeZWr3L";
+         }
+          break;
+      case "rap":
+        if (green > blue){
+          trackId = "6B2RPIeLqJyzZpR4334BWk";
+        } else if (green < blue){
+          trackId = "3fyMH1t6UPeR5croea9PrR";
+        } else {
+          trackId = "2wBCrtJS3E3TimRZ5MElTI";
+        }
+        break;
+      case "pop":
+        if (green > blue){
+          trackId = "455AfCsOhhLPRc68sE01D8";
+        } else if (green < blue){
+          trackId = "0GO8y8jQk1PkHzS31d699N";
+        } else {
+          trackId = "0GO8y8jQk1PkHzS31d699N"
+        }
+        break;
+      case "country":
+        if (green > blue){
+          trackId = "5fSPbm5lcwtqwXkeQQswW8"
+        } else if (green < blue){
+          trackId = "1WzAeadSKJhqykZFbJNmQv"
+        } else {
+          trackId = "0V4qXGKnZ6mXnWXDpda8d2"
+        }
+        break;
+      case "dance":
+        if (green > blue){
+          trackId = "6BtC99zhdbWXPGgEOXTTvw"
+        } else if ( green < blue){
+          trackId = "4jacsL77ZYnpInmTtUBaJW"
+        } else {
+          trackId = "5M0DX04ViGG3T1NXiB9EOu"
+        }
+
+    }
 
 
     var url = "https://api.spotify.com/v1/tracks/" + trackId
--- public/js/rgb-calc.js
+++ public/js/rgb-calc.js
@@ -1,8 +1,1 @@
-// $(document).ready(function(){
 
-
-//   console.log($("#ember241").text());
-
-
-// });
-// var trackId = "323vx2loX6l7g5wwWzlU1m"
